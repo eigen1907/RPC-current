dd36779e5f366ac7370c968e70779680a4c53988
eigen1907 <eigen1907@gmail.com>
2021-03-02 21:56:31 +0900

    no message

--- ShinJongWon/Fitting/fitting_by_run.py
+++ ShinJongWon/Fitting/fitting_by_run.py
@@ -1,25 +1,36 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
 
-dataset = pd.read_csv("/Users/mainroot/RPC-test_data/2017_addrun/dpid_323_2017.csv")
+path = "/Users/mainroot/RPC-test_data/2016_sep"
+dpidList = os.listdir(path)
 
+print(len(dpidList))
 
+for dpid in dpidList:
+    fillList = os.listdir(f"{path}/{dpid}")
+    for fill in fillList:
+        dataset = pd.read_csv(f"{path}/{dpid}/{fill}")
+        if len(dataset) > 50:
+            groups = dataset.groupby("run_number")
+            
+            fig, ax = plt.subplots(figsize=[16, 10])
+            for name, group in groups:
+                ax.plot(
+                    group.Imon_change_date,
+                    group.Imon,
+                    marker = "o",
+                    linestyle = "",
+                    label = str(name)[0:-2]
+                )
+            ax.legend(fontsize=12, loc="upper left")
+            plt.title("Scatter by Fill, Colered by Run", fontsize=20)
+            plt.xticks(np.arange(dataset.iloc[0][0], dataset.iloc[-1][0], step=(dataset.iloc[0][0]-dataset.iloc[-1][0])/5))
+            plt.yticks()
+            plt.xlabel("Imon_change_date", fontsize=12)
+            plt.ylabel("Imon", fontsize=12)
+            plt.show()
 
-is_5950 = dataset["fill_number"] == 5950
-
-fill_5950 = dataset[is_5950]
-
-fill_5950.plot(
-    kind = "scatter",
-    x = "Imon_change_date",
-    y = "Imon",
-    s = 10.0,
-    c = fill_5950["run_number"],
-    colormap = "rainbow"
-)
-
-
-plt.show()
 
